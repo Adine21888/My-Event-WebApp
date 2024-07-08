f78c8178a571e7d304488571fe68d9d9cf26c204
Dineth <dinethanjitha21@gmail.com>
2024-07-08 10:55:35 +0530

    cors headers

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ app.use(
     cors({
         origin: 'https://mern-web-app-frontend.vercel.app',
         methods: ['GET', 'POST', 'PUT', 'DELETE'],
-        allowedHeaders: ['Content-Type', 'Authorization'],
+        // allowedHeaders: ['Content-Type', 'Authorization'],
         credentials: true
     })
 );
